17a6cdac425bb3a068fcefc54bd01a71b8617bed
RHShoumik <rhshoumik12@gmail.com>
2022-06-12 11:04:41 +0600

    Show a single post in a page with unique url

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -26,18 +27,22 @@ app.get('/', (req, res) => {
 
 });
 app.get('/posts/:types', (req,res) => {
-  //console.log("Length : " + posts.length);
+
+  const requestedTitle = _.lowerCase(req.params.types);
+
   posts.forEach(item => {
-    //console.log("Params outside: " + req.params.types);
-    if(req.params.types == item.title)
+    const storedTitle = _.lowerCase(item.title);
+    if(storedTitle === requestedTitle)
     {
       console.log("Matched : " + req.params.types);
+      res.render('post' , {title: item.title , content : item.content}) 
     }
     else{
       console.log("Dont Matched : " + req.params.types);
+      res.redirect("/")
     }
   })
-  res.redirect("/")
+  //res.redirect("/")
 })
 app.get('/about', (req, res) =>{
   res.render('about', {abContent : aboutContent});
